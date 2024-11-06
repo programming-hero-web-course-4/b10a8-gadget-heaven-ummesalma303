c379ba88d08ee6f0e6a981c1191487feba6d71c5
ummesalma303 <ummesalma333999@gmail.com>
2024-11-06 21:16:38 +0600

    Added button in error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ErrorPage = () => {
+    const navigate=useNavigate()
     return (
         <div className='flex justify-center items-center h-screen text-center'>
             <div>
             <h1 className="text-8xl text-colorPrimary font-bold">404</h1>
-            <p>Data Not Found</p>
+                <p>Data Not Found</p>
+                <button onClick={()=>navigate('/')} className='btn text-white bg-colorPrimary'>Go Home</button>
             </div>
         </div>
     );
